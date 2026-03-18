f9c888c300bbb7e731708187cec96b7c4ddcb609
Rafael Alves <rafqel.alves@hotmail.com>
2026-03-18 14:49:58 -0300

    firstCommit

---
+++ script.js
@@ -0,0 +1,56 @@
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogWalked = "true";
+
+      if (dogWalked) {
+        resolve("You walk the dog 🐕");
+      } else {
+        reject("You DIDN'T WALK THE DOG.");
+      }
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const kitchenCleaned = false;
+
+      if (kitchenCleaned) {
+        resolve("You clean the kitchen 🧹");
+      } else {
+        reject("You DIDN'T clean the kitchen");
+      }
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const trashTakenOut = true;
+
+      if (trashTakenOut) {
+        resolve("You take out the trash ♻️");
+      } else {
+        reject("You DIDN'T take out the trash");
+      }
+    }, 500);
+  });
+}
+
+walkDog()
+  .then((value) => {
+    console.log(value);
+    return cleanKitchen();
+  })
+  .then((value) => {
+    console.log(value);
+    return takeOutTrash();
+  })
+  .then((value) => {
+    console.log(value);
+    console.log("You finished all the chores!");
+  })
+  .catch((error) => console.error(error));
